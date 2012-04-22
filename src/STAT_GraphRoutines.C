571f26b2d424dbf6cf084bf8bd30acd4f3044125
Gregory L. Lee <lee218@rzedgelet5.llnl.gov>
2012-04-21 17:55:55 -0700

    Added STAT Graphlib 2.0 support routines.

---
+++ src/STAT_GraphRoutines.C
@@ -0,0 +1,385 @@
+#include "STAT_GraphRoutines.h"
+
+graphlib_functiontable_p statBitVectorFunctions;
+graphlib_functiontable_p statMergeFunctions;
+graphlib_functiontable_p statReorderFunctions;
+int statGraphRoutinesTotalWidth;
+int *statGraphRoutinesEdgeLabelWidths;
+int statGraphRoutinesCurrentIndex;
+int *statGraphRoutinesRanksList;
+int statGraphRoutinesRanksListLength;
+
+void statInitializeBitVectorFunctions()
+{
+    statBitVectorFunctions = (graphlib_functiontable_p)malloc(sizeof(graphlib_functiontable_t));
+    if (statBitVectorFunctions == NULL)
+        fprintf(stderr, "Failed to malloc function table\n");
+    statBitVectorFunctions->serialize_node = statSerializeNode;
+    statBitVectorFunctions->serialize_node_length = statSerializeNodeLength;
+    statBitVectorFunctions->deserialize_node = statDeserializeNode;
+    statBitVectorFunctions->node_to_text = statNodeToText;
+    statBitVectorFunctions->merge_node = statMergeNode;
+    statBitVectorFunctions->copy_node = statCopyNode;
+    statBitVectorFunctions->free_node = statFreeNode;
+    statBitVectorFunctions->serialize_edge = statSerializeEdge;
+    statBitVectorFunctions->serialize_edge_length = statSerializeEdgeLength;
+    statBitVectorFunctions->deserialize_edge = statDeserializeEdge;
+    statBitVectorFunctions->edge_to_text = statEdgeToText;
+    statBitVectorFunctions->merge_edge = statMergeEdge;
+    statBitVectorFunctions->copy_edge = statCopyEdge;
+    statBitVectorFunctions->free_edge = statFreeEdge;
+    statBitVectorFunctions->edge_checksum = statEdgeCheckSum;
+}
+
+void statInitializeMergeFunctions()
+{
+    statMergeFunctions = (graphlib_functiontable_p)malloc(sizeof(graphlib_functiontable_t));
+    if (statMergeFunctions == NULL)
+        fprintf(stderr, "Failed to malloc function table\n");
+    statMergeFunctions->serialize_node = statSerializeNode;
+    statMergeFunctions->serialize_node_length = statSerializeNodeLength;
+    statMergeFunctions->deserialize_node = statDeserializeNode;
+    statMergeFunctions->node_to_text = statNodeToText;
+    statMergeFunctions->merge_node = statMergeNode;
+    statMergeFunctions->copy_node = statCopyNode;
+    statMergeFunctions->free_node = statFreeNode;
+    statMergeFunctions->serialize_edge = statSerializeEdge;
+    statMergeFunctions->serialize_edge_length = statSerializeEdgeLength;
+    statMergeFunctions->deserialize_edge = statFilterDeserializeEdge;
+    statMergeFunctions->edge_to_text = statEdgeToText;
+    statMergeFunctions->merge_edge = statMergeEdge;
+    statMergeFunctions->copy_edge = statCopyEdge;
+    statMergeFunctions->free_edge = statFreeEdge;
+    statMergeFunctions->edge_checksum = statEdgeCheckSum;
+}
+
+void statInitializeReorderFunctions()
+{
+    statReorderFunctions = (graphlib_functiontable_p)malloc(sizeof(graphlib_functiontable_t));
+    if (statReorderFunctions == NULL)
+        fprintf(stderr, "Failed to malloc function table\n");
+    statReorderFunctions->serialize_node = statSerializeNode;
+    statReorderFunctions->serialize_node_length = statSerializeNodeLength;
+    statReorderFunctions->deserialize_node = statDeserializeNode;
+    statReorderFunctions->node_to_text = statNodeToText;
+    statReorderFunctions->merge_node = statMergeNode;
+    statReorderFunctions->copy_node = statCopyNode;
+    statReorderFunctions->free_node = statFreeNode;
+    statReorderFunctions->serialize_edge = statSerializeEdge;
+    statReorderFunctions->serialize_edge_length = statSerializeEdgeLength;
+    statReorderFunctions->deserialize_edge = statDeserializeEdge;
+    statReorderFunctions->edge_to_text = statEdgeToText;
+    statReorderFunctions->merge_edge = statMergeEdgeOrdered;
+    statReorderFunctions->copy_edge = statCopyEdgeInitializeEmpty;
+    statReorderFunctions->free_edge = statFreeEdge;
+    statReorderFunctions->edge_checksum = statEdgeCheckSum;
+}
+
+size_t statBitVectorLength(int numTasks)
+{
+    int ret;
+
+    ret = numTasks / STAT_BITVECTOR_BITS;
+    if (numTasks % STAT_BITVECTOR_BITS != 0)
+        ret += 1;
+    return ret;
+}
+
+void statSerializeNode(char *buf, const void *node)
+{
+  if (node!= NULL)
+    strcpy(buf, (char *)node);
+}
+
+unsigned int statSerializeNodeLength(const void *node)
+{
+  if (node!=NULL)
+    return strlen((char *)node) + 1;
+  else
+    return 0;
+}
+
+void statDeserializeNode(void **node, const char *buf, unsigned int bufLength)
+{
+  *node = malloc(bufLength);
+  strcpy((char *)*node, buf);
+}
+
+char *statNodeToText(const void *node)
+{
+    return (char *)node;
+}
+
+void statMergeNode(void *node1, const void *node2)
+{
+    // No Op
+}
+
+void *statCopyNode(const void *node)
+{
+    return (void *)strdup((char *)node);
+}
+
+void statFreeNode(void *node)
+{
+    free(node);
+}
+
+void statSerializeEdge(char *buf, const void *edge)
+{
+    char *ptr = buf;
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge;
+
+    memcpy(ptr, (void *)&(e->length), sizeof(size_t));
+    ptr += sizeof(size_t);
+    memcpy(ptr, e->bitVector, STAT_BITVECTOR_BYTES * e->length);
+}
+
+unsigned int statSerializeEdgeLength(const void *edge)
+{
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge;
+    return sizeof(size_t) + STAT_BITVECTOR_BYTES * e->length;
+}
+
+void statDeserializeEdge(void **edge, const char *buf, unsigned int bufLength)
+{
+    char *ptr = (char *)buf;
+    StatBitVectorEdge_t *e;
+
+    e = (StatBitVectorEdge_t *)malloc(sizeof(StatBitVectorEdge_t));
+    if (e == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for deserialized edge object\n", sizeof(StatBitVectorEdge_t));
+        return;
+    }
+
+    memcpy((void *)&(e->length), ptr, sizeof(size_t));
+    ptr += sizeof(size_t);
+    e->bitVector = (int64_t *)malloc(STAT_BITVECTOR_BYTES * e->length);
+    if (e == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for deserialized edge bit vector\n", STAT_BITVECTOR_BYTES * e->length);
+        return;
+    }
+    memcpy(e->bitVector, ptr, STAT_BITVECTOR_BYTES * e->length);
+    *edge = (void *)e;
+}
+
+char *statEdgeToText(const void *edge)
+{
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge;
+    char val[128];
+    int i, j;
+    int in_range = 0, first_iteration = 1, range_start, range_end, cur_val, last_val = 0;
+    char *ret;
+    unsigned int ret_size = 0, count = 0;
+  
+    ret = (char *)malloc(STAT_GRAPH_CHUNK * sizeof(char));
+    ret_size = STAT_GRAPH_CHUNK;
+    if (ret == NULL)
+    {
+        fprintf(stderr, "%s: Failed to allocte memory for edge label\n", strerror(errno));
+        return NULL;
+    }
+    sprintf(ret + count, "[");
+    count += 1;
+    for (i = 0; i< e->length; i++)
+    {
+        if (ret_size - count < 1024)
+        {
+            /* Reallocate if we are within 1024 bytes of the end */
+            /* This is a large threshold to keep it out of the innter loop */
+            ret_size += STAT_GRAPH_CHUNK;
+            ret = (char *)realloc(ret, ret_size * sizeof(char));
+            if (ret == NULL)
+            {
+                fprintf(stderr, "%s: Failed to reallocte %d bytes of memory for edge label\n", strerror(errno), ret_size);
+                return NULL;
+            }
+        }
+        for (j = 0; j < 8 * sizeof(int64_t); j++)
+        {
+            if (e->bitVector[i] & STAT_GRAPH_BIT(j))
+            {
+                cur_val = i * 8 * sizeof(int64_t) + j;
+                if (in_range == 0)
+                {
+                    snprintf(val, 128, "%d", cur_val);
+                    if (first_iteration == 0)
+                    {
+                        if (cur_val == last_val + 1)
+                        {
+                            in_range = 1;
+                            range_start = last_val;
+                            range_end = cur_val;
+                            sprintf(ret + count, "-");
+                            count += 1;
+                        }        
+                        else
+                        {
+                            sprintf(ret + count, ",");
+                            count += 1;
+                            sprintf(ret + count, "%s", val);
+                            count += strlen(val);
+                        }
+                    }
+                    else
+                    {
+                        sprintf(ret + count, "%s", val);
+                        count += strlen(val);
+                    }
+                }
+                else
+                {
+                    if (cur_val == last_val + 1)
+                        range_end = cur_val;
+                    else
+                    {
+                        snprintf(val, 128, "%d,%d", last_val, cur_val);
+                        sprintf(ret + count, "%s", val);
+                        count += strlen(val);
+                        in_range = 0;
+                    }
+                }
+                first_iteration = 0;
+                last_val = cur_val;
+            }
+        }
+    }
+    if (in_range == 1)
+    {
+        snprintf(val, 128, "%d", last_val);
+        sprintf(ret + count, "%s", val);
+        count += strlen(val);
+    }
+    sprintf(ret + count, "]");
+    count += 1;
+
+    return ret;
+}
+
+void statMergeEdge(void *edge1, const void *edge2)
+{
+    unsigned int i;
+    StatBitVectorEdge_t *e1 = (StatBitVectorEdge_t *)edge1, *e2 = (StatBitVectorEdge_t *)edge2;
+
+    for (i = 0; i < e1->length; i++)
+        e1->bitVector[i] |= e2->bitVector[i];
+}
+
+void *statCopyEdge(const void *edge)
+{
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge, *ret;
+
+    ret = (StatBitVectorEdge_t *)malloc(sizeof(StatBitVectorEdge_t));
+    if (ret == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for edge copy\n", sizeof(StatBitVectorEdge_t));
+        return NULL;
+    }
+    ret->length = e->length;
+    ret->bitVector = (int64_t *)malloc(e->length * STAT_BITVECTOR_BYTES);
+    if (ret->bitVector == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for bit vector\n", e->length * STAT_BITVECTOR_BYTES);
+        return NULL;
+    }
+    memcpy(ret->bitVector, e->bitVector, STAT_BITVECTOR_BYTES * e->length);
+    return (void *)ret;
+}
+
+void statFreeEdge(void *edge)
+{
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge;
+
+    if (e->bitVector != NULL)
+        free(e->bitVector);
+    free(e);
+}
+
+long statEdgeCheckSum(const void *edge)
+{
+    StatBitVectorEdge_t *e = (StatBitVectorEdge_t *)edge;
+    int i;
+    long ret = 0;
+
+    for (i = 0; i < e->length; i++)
+        ret = ret + e->bitVector[i] * (e->length - i + 1);
+        
+    return ret;
+}
+
+void statFilterDeserializeEdge(void **edge, const char *buf, unsigned int bufLength)
+{
+    int offset, i;
+    char *ptr = (char *)buf;
+    size_t currentEdgeLength;
+    StatBitVectorEdge_t *e;
+
+    e = (StatBitVectorEdge_t *)malloc(sizeof(StatBitVectorEdge_t));
+    if (e == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for deserialized edge object\n", sizeof(StatBitVectorEdge_t));
+        return;
+    }
+
+    memcpy((void *)&(currentEdgeLength), ptr, sizeof(size_t));
+    ptr += sizeof(size_t);
+    e->length = statGraphRoutinesTotalWidth;
+    e->bitVector = (int64_t *)calloc(e->length, STAT_BITVECTOR_BYTES);
+    if (e == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for deserialized edge bit vector\n", STAT_BITVECTOR_BYTES * e->length);
+        return;
+    }
+
+    offset = 0;
+    for (i = 0; i < statGraphRoutinesCurrentIndex; i++)
+        offset += statGraphRoutinesEdgeLabelWidths[i];
+
+    memcpy((void *)&(e->bitVector[offset]), ptr, STAT_BITVECTOR_BYTES * currentEdgeLength);
+    *edge = (void *)e;
+}
+
+void *statCopyEdgeInitializeEmpty(const void *edge)
+{
+    StatBitVectorEdge_t *ret;
+
+    ret = (StatBitVectorEdge_t *)malloc(sizeof(StatBitVectorEdge_t));
+    if (ret == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for edge copy\n", sizeof(StatBitVectorEdge_t));
+        return NULL;
+    }
+    ret->length = statGraphRoutinesTotalWidth;
+    ret->bitVector = (int64_t *)calloc(ret->length, STAT_BITVECTOR_BYTES);
+    if (ret->bitVector == NULL)
+    {
+        fprintf(stderr, "Failed to allocate %u bytes for bit vector\n", ret->length * STAT_BITVECTOR_BYTES);
+        return NULL;
+    }
+    return (void *)ret;
+}
+
+int bitVectorContains(int64_t *vec, int val)
+{
+    return !!(vec[val / STAT_BITVECTOR_BITS] & STAT_GRAPH_BIT(val % STAT_BITVECTOR_BITS));
+}
+
+void statMergeEdgeOrdered(void *edge1, const void *edge2)
+{
+    unsigned int i;
+    int bit, byte;
+
+    StatBitVectorEdge_t *e1 = (StatBitVectorEdge_t *)edge1, *e2 = (StatBitVectorEdge_t *)edge2;
+
+    for (i = 0; i < statGraphRoutinesRanksListLength; i++)
+    {
+        if (bitVectorContains(e2->bitVector, statGraphRoutinesCurrentIndex * STAT_BITVECTOR_BITS + i) == 1)
+        {
+            byte = statGraphRoutinesRanksList[i] / STAT_BITVECTOR_BITS;
+            bit = statGraphRoutinesRanksList[i] % STAT_BITVECTOR_BITS;
+            e1->bitVector[byte] |= STAT_GRAPH_BIT(bit);
+        }
+    }
+}
---
+++ src/STAT_GraphRoutines.h
@@ -0,0 +1,168 @@
+#include "graphlib.h"
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifndef STAT_GRAPHROUTINES_H
+#define STAT_GRAPHROUTINES_H
+
+#define STAT_BITVECTOR_BITS 64 
+#define STAT_BITVECTOR_BYTES 8
+#define STAT_GRAPH_BIT(i) ((int64_t)1<<(i))
+#define STAT_GRAPH_CHUNK 8192
+
+//! A struct to specify STAT's bit vector edge label type
+typedef struct
+{
+    size_t length;
+    int64_t *bitVector;
+} StatBitVectorEdge_t;
+
+//! Initialize the standard bit vector functions
+void statInitializeBitVectorFunctions();
+
+//! Initialize the merge bit vector functions for the filter
+void statInitializeMergeFunctions();
+
+//! Initialize the bit vector reorder functions for the frontend
+void statInitializeReorderFunctions();
+
+//! Calculate the bit vector length
+/*!
+    \param numTasks - the number of tasks to represent in the bit vector
+    \return the length of the bit vector (i.e., number of int64_t elements)
+*/
+size_t statBitVectorLength(int numTasks);
+
+//! Serialize the STAT node object into a buffer
+/*!
+    \param buf - the buffer to write to
+    \param node - a pointer to the node object
+*/
+void statSerializeNode(char *buf, const void *node);
+
+//! Calculate the serialized length of the STAT node object
+/*!
+    \param node - a pointer to the node object
+    \return the serialized length of the node
+*/
+unsigned int statSerializeNodeLength(const void *node);
+
+//! Deserialize the STAT node object from a buffer
+/*!
+    \param[out] node - a pointer to the node object
+    \param buf - the serialized buffer
+    \param bufLength - the length of the serialized buffer
+*/
+void statDeserializeNode(void **node, const char *buf, unsigned int bufLength);
+
+//! Translate the STAT node object into a string
+/*!
+    \param node - a pointer to the node object
+    \return the string representation of the node
+*/
+char *statNodeToText(const void *node);
+
+//! Merge two STAT node objects
+/*!
+    \param[inout] node1 - the node to merge into
+    \param node2 - the node to merge in
+*/
+void statMergeNode(void *node1, const void *node2);
+
+//! Copy a STAT node object
+/*!
+    \param node - a pointer to the node object to be copied
+    \return a pointer to the new node copy
+*/
+void *statCopyNode(const void *node);
+
+//! Free a STAT node object
+/*!
+    \param node - a pointer to the node object to be freed
+*/
+void statFreeNode(void *node);
+
+//! Serialize a STAT edge object into a buffer
+/*!
+    \param buf - the buffer to write to
+    \param edge - a pointer to the edge object
+*/
+void statSerializeEdge(char *buf, const void *edge);
+
+//! Calculate the serialized length of a STAT edge object
+/*!
+    \param edge - a pointer to the edge object
+    \return the serialized length of the edge
+*/
+unsigned int statSerializeEdgeLength(const void *edge);
+
+//! Deserialize a STAT edge object from a buffer
+/*!
+    \param[out] edge - a pointer to the edge object
+    \param buf - the serialized buffer
+    \param bufLength - the length of the serialized buffer
+*/
+void statDeserializeEdge(void **edge, const char *buf, unsigned int bufLength);
+
+//! Translate a STAT edge object into a string
+/*!
+    \param edge - a pointer to the edge object
+    \return the string representation of the edge
+*/
+char *statEdgeToText(const void *edge);
+
+//! Merge two STAT edge objects
+/*!
+    \param[inout] edge1 - the edge to merge into
+    \param edge2 - the edge to merge in
+*/
+void statMergeEdge(void *edge1, const void *edge2);
+
+//! Copy a STAT edge object
+/*!
+    \param edge - a pointer to the edge object to be copied
+    \return a pointer to the new edge copy
+*/
+void *statCopyEdge(const void *edge);
+
+//! Free a STAT edge object
+/*!
+    \param edge - a pointer to the edge object to be freed
+*/
+void statFreeEdge(void *edge);
+
+//! Calculate a checksum of a STAT edge object
+/*!
+    \param edge - a pointer to the edge object
+    \return the checksum of the edge
+*/
+long statEdgeCheckSum(const void *edge);
+
+//! Deserialize a STAT edge object from a buffer into the appropriate location in the bit vector for the STAT filter.This requires statGraphRoutinesTotalWidth must be set to the final bit vector width, statGraphRoutinesEdgeLAbelWidths must be a list of bit vector widths, and statGraphRoutinesCurrentIndex must be set to the current index.
+/*!
+    \param[out] edge - a pointer to the edge object
+    \param buf - the serialized buffer
+    \param bufLength - the length of the serialized buffer
+*/
+void statFilterDeserializeEdge(void **edge, const char *buf, unsigned int bufLength);
+
+
+//! Initialize an empty bit vector for initializing the frontend's reorder graph
+/*!
+    \param edge - a pointer to the edge object to be copied
+    \return a pointer to the new edge copy
+*/
+void *statCopyEdgeInitializeEmpty(const void *edge);
+
+
+//! Merge the unordered input vector bits into the appropriate in-order bits of the frontend's reorder graph. This requires statGraphRoutinesRanksList must be set to the ranks list for the current bitvector, statGraphRoutinesRanksListLength must be set to the length of the ranks list for the current bitvector, and statGraphRoutinesCurrentIndex must be set to the start index for the bit vector in edge
+/*!
+    \param[inout] edge1 - the edge to merge into
+    \param edge2 - the edge to merge in
+*/
+void statMergeEdgeOrdered(void *edge1, const void *edge2);
+
+#endif
