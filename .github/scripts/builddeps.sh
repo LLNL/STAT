35b140bc0c5bdbd4a8f5609da8f749fe3f633063
Gregory L. Lee <lee218@llnl.gov>
2021-04-30 13:55:09 -0700

    initial actions added

--- .github/scripts/builddeps.sh
+++ .github/scripts/builddeps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/LLNL/graphlib/archive/refs/tags/v3.0.0.tar.gz
-tar xf graphlib-3.0.0.tar.gz
+tar xf v3.0.0.tar.gz
 pushd graphlib-3.0.0
     mkdir build
     pushd build
