2226a74ec4925cc9826a0e855ce2f389cb6042c0
Gregory L. Lee <lee218@llnl.gov>
2021-05-03 09:41:10 -0700

    initial actions added

--- .github/scripts/builddeps.sh
+++ .github/scripts/builddeps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -x
 
 # download and build dyninst
-wget https://github.com/dyninst/dyninst/archive/refs/tags/v11.0.0.tar.gz
+wget -q https://github.com/dyninst/dyninst/archive/refs/tags/v11.0.0.tar.gz
 tar xf v11.0.0.tar.gz
 pushd dyninst-11.0.0
     mkdir build
@@ -23,7 +24,7 @@ exit 0
 export CC="gcc"
 export CXX="g++"
 # download and build graphlib
-wget https://github.com/LLNL/graphlib/archive/refs/tags/v3.0.0.tar.gz
+wget -q https://github.com/LLNL/graphlib/archive/refs/tags/v3.0.0.tar.gz
 tar xf v3.0.0.tar.gz
 pushd graphlib-3.0.0
     mkdir build
@@ -37,7 +38,7 @@ ls -l $HOME/local/
 ls -l $HOME/local/lib
 
 # download and build mrnet
-#wget https://github.com/dyninst/mrnet/archive/refs/tags/v5.0.1.tar.gz
+#wget -q https://github.com/dyninst/mrnet/archive/refs/tags/v5.0.1.tar.gz
 #tar xf v5.0.1.tar.gz
 #pushd mrnet-5.0.1
 git clone https://github.com/dyninst/mrnet
