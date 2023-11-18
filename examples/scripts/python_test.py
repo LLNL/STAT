d98bd8b072ac9792000b95fff45398bdb78f9a5a
Gregory L. Lee <lee218@llnl.gov>
2023-11-17 17:29:14 -0800

    adding threads to python example

--- examples/scripts/python_test.py
+++ examples/scripts/python_test.py
@@ -1,8 +1,18 @@
+import logging
+import threading
+import time
 has_mpi4py = True
 try:
     from mpi4py import MPI
 except:
     has_mpi4py = false
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    for i in range(120):
+        time.sleep(5)
+    logging.info("Thread %s: finishing", name)
+
 
 def foo(rank):
     if rank == 0:
@@ -18,8 +28,21 @@ def foo2(rank):
 
 def bar(rank):
     import time
-    for i in range(120):
-        time.sleep(5)
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for index in range(3):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
     print('done sleeping, importing parser')
     import parser
     print ('importing socket')
